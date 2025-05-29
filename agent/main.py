457eab8446679bb6c01934b34405e195b16fff04
Stuart Manley <true911-pi@manleysolutions.com>
2025-05-28 21:01:03 -0400

    Initial commit for NG911 Pi appliance

---
+++ agent/main.py
@@ -0,0 +1,14 @@
+import time
+print("🔐 Agent-Core started: registering device...
+
+cd ~/true911
+docker-compose up -d
+
+docker-compose logs -f
+cd ~/true911
+docker ps
+
+cd ~/true911
+docker-compose up -d
+
+docker-compose logs -f
